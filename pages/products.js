c4656edb1a80a2e9a43c18adfdb3efb761516943
shawneily <shawn.neily@icloud.com>
2023-08-13 23:11:28 -0600

    add products page

---
+++ pages/products.js
@@ -0,0 +1,15 @@
+const Products = () => {
+    return (
+        <>
+        <h1>Product One</h1>
+        <p>This is a Products</p>
+        <p>$20.00</p>
+        <hr />
+        <h1>Product Two</h1>
+        <p>This is another product.</p>
+        <p>$35.00</p>
+        </>
+    );
+}
+
+export default Products;
